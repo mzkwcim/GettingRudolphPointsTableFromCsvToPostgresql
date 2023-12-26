171c5df4100b9598b66d77dfb7aa311e85ebb4e7
M5kwc1m <144521687+mzkwcim@users.noreply.github.com>
2023-12-26 22:51:17 +0100

    Update Program.cs
    Mainly ConvertStringToDouble function has been changed due to the problems with csv file reading. To Fully automate parsing and downloading of rudolph table i should incorporate excel into this process.

--- CSVProject/Program.cs
+++ CSVProject/Program.cs
@@ -1,4 +1,4 @@
-﻿using CsvHelper;
+using CsvHelper;
 using Npgsql;
 using System.Globalization;
 using System.IO;
@@ -8,80 +8,80 @@ class Project
 {
     static void Main()
     {
-        string filePath = "C:\\Users\\Laptop\\Desktop\\RudolphTable2023_10YearOldMen.csv";
+        string filePath = "C:\\Users\\mzkwcim\\Desktop\\RudolphTable\\RudolphTable_Girls_Open.csv";
+        List<string> listOfFiles = new List<string>();
         List<string> collumnNames = new List<string>();
         List<string> tableValues = new List<string>();
-        string name = "RudolphTable10YearsOldBoys";
+        string name = "RudolphTableopengirls";
         int adder = 0;
         using (var reader = new StreamReader(filePath))
         {
-            while(!reader.EndOfStream)
+            while (!reader.EndOfStream)
             {
                 var line = reader.ReadLine();
-                var values = line.Split(";");
+                var values = line.Split(",");
                 if (adder == 0)
                 {
                     for (int i = 0; i < values.Length; i++)
                     {
-                        collumnNames.Add(values[i]);
+                        collumnNames.Add(values[i].Replace("\"",""));
+                        Console.WriteLine(values[i]);
                     }
                 }
                 else
                 {
                     for (int i = 0; i < values.Length; i++)
                     {
-                        tableValues.Add(values[i]);
+                        tableValues.Add(values[i].Replace("\"",""));
                     }
                 }
-                
+
                 adder++;
             }
         }
+
         Connection(name, collumnNames, tableValues);
     }
-
     static List<double> ConvertStringToDouble(List<string> tableValues)
     {
         List<double> converted = new List<double>();
         for (int i = 0; i < tableValues.Count; i++)
         {
-            string [] list = tableValues[i].Split(":");
-            converted.Add((list.Length != 1) ? (Math.Round(Convert.ToDouble(list[0]), 2) * 60 + Math.Round(Convert.ToDouble(list[1]), 2)) : Math.Round(Convert.ToDouble(list[0]), 2));
+            string[] list = tableValues[i].Split(":");
+            converted.Add((i % 18 == 0) ? Convert.ToDouble(tableValues[i]) : (list[0] != "00") ? (Math.Round(Convert.ToDouble(list[0].Replace("\'", "")), 2) * 60 + Math.Round(Convert.ToDouble(list[1].Replace("\'", "")), 2)) : Math.Round(Convert.ToDouble(list[1].Replace("\'", "")), 2));
         }
         return converted;
     }
     static string CreateTable(string name, List<string> collumnNames)
     {
         string createTableQueryTest = $"CREATE TABLE {name} (" +
-                          "ID SERIAL PRIMARY KEY," +
-                          " pkt INTEGER, ";                
-        for (int  i = 0; i < 17; i++)
+                          "ID SERIAL PRIMARY KEY,";
+        for (int i = 0; i < collumnNames.Count; i++)
         {
-            createTableQueryTest += (i != 16) ? ($"{collumnNames[i + 1].ToLower()} DOUBLE PRECISION, ") : ($"\"{collumnNames[i + 1].ToLower()}\" DOUBLE PRECISION ");
+            createTableQueryTest += (i != collumnNames.Count -1) ? $"{collumnNames[i].ToLower()} DOUBLE PRECISION, " : $"{collumnNames[i].ToLower()} DOUBLE PRECISION );";
         }
-        createTableQueryTest += ");";
         Console.WriteLine(createTableQueryTest);
         return createTableQueryTest;
     }
     static string AddValuesToQuery(string name, List<string> collumnNames, List<string> tableValues)
     {
+        Console.WriteLine("Jeszcze nie");
         List<double> doubles = ConvertStringToDouble(tableValues);
         Console.WriteLine("tutaj?");
         string AddValues = $"INSERT INTO {name} (";
         for (int j = 0; j < collumnNames.Count; j++)
         {
-            AddValues += (j < collumnNames.Count - 1) ? $"{collumnNames[j]}, " : $"{collumnNames[j]} ) VALUES ";
+            AddValues += (j < collumnNames.Count - 1) ? $" {collumnNames[j]}, " : $" {collumnNames[j]} ) VALUES ";
         }
-
         for (int i = 0; i < 20; i++)
         {
-            AddValues += "(";
-            for (int j = 0; j < 18; j++)
+            AddValues += $"( {20-i}, ";
+            for (int j = 1; j < 18; j++)
             {
-                AddValues += (j < 17) ? $"{doubles[(i * 18) + j]}, " : $"{doubles[(i * 18) + j]} ";
-                Console.WriteLine(doubles[(i*18)+j]);
+                AddValues += (j < 17) ? $"{Math.Round(doubles[(i * 18) + j],2)}, " : $"{Math.Round(doubles[(i * 18) + j],2)} ";
+                Console.WriteLine(doubles[(i * 18) + j]);
             }
-            AddValues += ((i  < 19)) ? " ), " : " ); ";
+            AddValues += (i < 19) ? " ), " : " ); ";
         }
         return AddValues;
     }
@@ -117,7 +117,7 @@ class Project
                     }
                 }
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 Console.WriteLine(ex.ToString());
             }
@@ -135,4 +135,3 @@ class Project
         }
     }
 }
-
